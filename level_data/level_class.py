d809926041c337903b6279924ac7dd32b712cf5d
Lena-hal <78831891+Lena-hal@users.noreply.github.com>
2023-03-20 19:47:05 +0100

    new textures, indexing script
    added few new textures, also added new script that helps me to index new textures easily

--- level_data/level_class.py
+++ level_data/level_class.py
@@ -1,11 +1,24 @@
 import texture_class
 import json
 class Level:
-    def __init__(self,level_path) -> None:
+    def __init__(self,level_path,game) -> None:
+        self.game = game
         with open(level_path,"r") as data:
             self.level_data = json.load(data)
+        self.background = Background(self.level_data["Data"]["Background"],self.game)
         for i in self.level_data["Data"]["Walls"]:
             print(i)
 
-Level("level_data/level1.json")
+
+
+class Background:
+    def __init__(self,path,game) -> None:
+        self.game = game
+        self._x = 0
+        self._y = 0
+        self.texture = texture_class.Texture_manager.get_texture(path)
+        print(self.texture)
+        self.game.render_list.append(self)
+
     
+Level("level_data/level1.json")
