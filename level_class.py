2294d66f0514a4531d69d34c478734ee986ebe23
Lena-hal <78831891+Lena-hal@users.noreply.github.com>
2023-03-24 21:34:43 +0100

    texture update

--- level_class.py
+++ level_class.py
@@ -4,11 +4,12 @@ import wall_class
 class Level:
     def __init__(self,level_path,game) -> None:
         self.game = game
+        self.walls = []
         with open(level_path,"r") as data:
             self.level_data = json.load(data)
         self.background = Background(self.level_data["Data"]["Background"],self.game)
         for i in self.level_data["Data"]["Walls"]:
-            print(i)
+            self.walls.append(wall_class.Wall(i[0],i[1],i[2],i[3],self.game,level_path,i[4]))
 
 
 
@@ -17,9 +18,7 @@ class Background:
         self.game = game
         self._x = 0
         self._y = 0
-        self.texture = texture_class.Texture_manager.get_texture(path)
+        self.texture = game.manager.get_texture(path)
         print(self.texture)
         self.game.render_list.append(self)
 
-    
-Level("level_data/level1.json")
--- wall_class.py
+++ wall_class.py
@@ -1,10 +1,10 @@
 import game_object
+import texture_class
 
 class Wall(game_object.Object):
-    def __init__(self,x,y,width,height,game,level):
+    def __init__(self,x,y,width,height,game,level,texture):
         self.layer = 0
-        texture = self.generate_texture()
+        self.texture = game.manager.get_texture(texture,)
         super().__init__(texture, level, x, y, width, height, self.layer, game)
-        
-    def generate_texture():
-        pass
+    
+
